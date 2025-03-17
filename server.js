cd6d3cfd09f557b317e33d5d7975347638225dd4
Giovanni Smith <giovanni020702@gmail.com>
2025-03-16 20:54:34 -0700

    More changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
-// this variable keeps track of the payments
+// this variable keeps track of the number of payments
 let paymentCounter = 0;
 
 // send homepage html file to the client
@@ -11,7 +11,34 @@ app.get('/', function (req, res) {
     const options = {
         root: path.join(__dirname)
     };
+    const fileName = 'homepage.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 
+    // by default, the code prints the current time every second. this is a relatively load low
+    setInterval(printCurrentTime, 1000);
+});
+
+// when the user submits the form, the payment variable increases and it is displayed back to the client
+app.post("/submit", (req, res) => {
+    paymentCounter++;
+    res.send(`<h1>Thanks. Payment number: ` + paymentCounter + `</h1>`);
+});
+
+// this is called when the user presses the "low load" button
+app.post("/lowLoad", (req, res) => {
+    // prints the current time every second. this is a relatively load low
+    setInterval(printCurrentTime, 1000);
+
+    // returns the current page to the client
+    const options = {
+        root: path.join(__dirname)
+    };
     const fileName = 'homepage.html';
     res.sendFile(fileName, options, function (err) {
         if (err) {
@@ -22,11 +49,31 @@ app.get('/', function (req, res) {
     });
 });
 
-// when the user submits the form, the payment variable increases and it is displayed back to the client
-app.post("/submit", (req, res) => {
-    paymentCounter++;
-    res.send(`<h1>Thanks. Payment number: ` + paymentCounter + `</h1>`);
+// calls a method every 1 miliseconds and redirects the user to the same page
+app.post("/highLoad", (req, res) => {
+    // prints the current time every milisecond. this is a relatively high low
+    setInterval(printCurrentTime, 1);
+
+    // returns the current page to the client
+    const options = {
+        root: path.join(__dirname)
+    };
+    const fileName = 'homepage.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 });
+
+// prints the current time to the console
+function printCurrentTime()
+{
+    var date_time = new Date();
+    console.log(date_time);
+}
 
 // server listens for new clients
 app.listen(PORT, function (err) {
